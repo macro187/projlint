0c14dca4f5d03c7a0b2426f12fdea12e2a3968fc
Ron MacNeil <macro@hotmail.com>
2019-11-14 22:57:12 +1000

    Add a gitignore aspect

---
+++ projlint/Aspects/A_GitIgnore_File.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using ProjLint.Contexts;
+
+namespace ProjLint.Aspects
+{
+    public class A_GitIgnore_File : RepositoryAspect
+    {
+
+        public A_GitIgnore_File(RepositoryContext context)
+            : base(context)
+        {
+            gitIgnorePath = Path.Combine(Context.Path, ".gitignore");
+        }
+
+
+        string gitIgnorePath;
+        bool isGitIgnorePresent;
+
+
+        protected override bool OnAnalyse()
+        {
+            return isGitIgnorePresent = File.Exists(gitIgnorePath);
+        }
+
+
+        protected override bool OnApply()
+        {
+            if (!isGitIgnorePresent)
+            {
+                using (File.Create(gitIgnorePath)) {}
+            }
+
+            return true;
+        }
+
+    }
+}
