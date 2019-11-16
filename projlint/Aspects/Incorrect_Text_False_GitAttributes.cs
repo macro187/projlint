b42a542301ac773b108c0378924a180dbc3131ef
Ron MacNeil <macro@hotmail.com>
2019-11-16 18:17:07 +1000

    Add Incorrect_Text_False_GitAttributes aspect

---
+++ projlint/Aspects/Incorrect_Text_False_GitAttributes.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using MacroIO;
+using ProjLint.Contexts;
+
+namespace ProjLint.Aspects
+{
+    public class Incorrect_Text_False_GitAttributes : RepositoryAspect
+    {
+
+        /// <summary>
+        /// What people sometimes incorrectly put in .gitattributes instead of "* -text"
+        /// </summary>
+        ///
+        const string IncorrectLine = "* text=false";
+
+
+        public Incorrect_Text_False_GitAttributes(RepositoryContext context)
+            : base(context)
+        {
+            Require<GitAttributes_File>();
+            gitAttributesPath = GitAttributes_File.GetGitAttributesPath(Context);
+        }
+
+
+        readonly string gitAttributesPath;
+        bool isLinePresent;
+
+
+        protected override bool OnAnalyse()
+        {
+            isLinePresent = File.ReadLines(gitAttributesPath).Any(line => line.Trim() == IncorrectLine);
+            return !isLinePresent;
+        }
+
+
+        protected override bool OnApply()
+        {
+            if (isLinePresent)
+            {
+                var correctLines =
+                    File.ReadLines(gitAttributesPath)
+                        .Where(line => line != IncorrectLine)
+                        .ToList();
+
+                FileExtensions.RewriteAllLines(gitAttributesPath, correctLines);
+            }
+
+            return true;
+        }
+
+    }
+}
