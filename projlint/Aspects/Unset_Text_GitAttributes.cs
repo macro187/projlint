ec44041d58d1ff0d33c4fe2af94644355befaca7
Ron MacNeil <macro@hotmail.com>
2019-11-16 18:16:52 +1000

    Add Unset_Text_GitAttributes aspect

---
+++ projlint/Aspects/Unset_Text_GitAttributes.cs
@@ -0,0 +1,14 @@
+﻿using ProjLint.Contexts;
+
+namespace ProjLint.Aspects
+{
+    public class Unset_Text_GitAttributes : GitAttributesAspect
+    {
+
+        public Unset_Text_GitAttributes(RepositoryContext context)
+            : base(context, "* -text")
+        {
+        }
+
+    }
+}
