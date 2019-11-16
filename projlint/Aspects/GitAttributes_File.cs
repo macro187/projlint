cbcfa245e6f5c5d50074f6adb90f70f6ef0616ca
Ron MacNeil <macro@hotmail.com>
2019-11-16 18:03:13 +1000

    Add GitAttributes_File aspect

---
+++ projlint/Aspects/GitAttributes_File.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using ProjLint.Contexts;
+
+namespace ProjLint.Aspects
+{
+    public class GitAttributes_File : RepositoryAspect
+    {
+
+        public static string GetGitAttributesPath(RepositoryContext context) =>
+            Path.Combine(context.Path, ".gitattributes");
+
+
+        public GitAttributes_File(RepositoryContext context)
+            : base(context)
+        {
+            gitAttributesPath = GetGitAttributesPath(Context);
+        }
+
+
+        readonly string gitAttributesPath;
+        bool isGitAttributesPresent;
+
+
+        protected override bool OnAnalyse()
+        {
+            return isGitAttributesPresent = File.Exists(gitAttributesPath);
+        }
+
+
+        protected override bool OnApply()
+        {
+            if (!isGitAttributesPresent)
+            {
+                using (File.Create(gitAttributesPath)) { }
+            }
+
+            return true;
+        }
+
+    }
+}
