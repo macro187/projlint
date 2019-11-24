f743412c370efc8cb2fba002618aabf724794725
Ron MacNeil <macro@hotmail.com>
2019-11-24 20:52:23 +1000

    Match No_Utf8_Boms files using regexes and check more

--- projlint/Aspects/No_Utf8_Boms.cs
+++ projlint/Aspects/No_Utf8_Boms.cs
@@ -1,7 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using MacroDiagnostics;
 using MacroIO;
 using ProjLint.Contexts;
 
@@ -10,12 +12,18 @@ namespace ProjLint.Aspects
     public class No_Utf8_Boms : RepositoryAspect
     {
 
-        static readonly string[] FilesThatShouldHaveNoBom =
+        static readonly Regex[] FilesThatShouldHaveNoBom =
             new []
             {
-                ".sln",
-                ".csproj",
-                ".cs",
+                new Regex(@"\.sln$"),
+                new Regex(@"\.csproj$"),
+                new Regex(@"\.cs$"),
+                new Regex(@"\.md$"),
+                new Regex(@"^\.gitignore$"),
+                new Regex(@"^\.gitattributes$"),
+                new Regex(@"^\.editorconfig$"),
+                new Regex(@"^\.nugit$"),
+                new Regex(@"^\.produce$"),
             };
 
 
@@ -43,7 +51,7 @@ namespace ProjLint.Aspects
                     continue;
                 }
 
-                Trace.TraceWarning(file);
+                Trace.TraceError($"UTF-8 BOM(s) in {file}");
                 filesWithBoms.Add(file);
             }
 
@@ -64,18 +72,18 @@ namespace ProjLint.Aspects
 
         bool ShouldHaveNoBom(string path)
         {
-            return
-                FilesThatShouldHaveNoBom
-                .Any(ext => path.EndsWith(ext, StringComparison.OrdinalIgnoreCase));
+            return FilesThatShouldHaveNoBom.Any(regex => regex.IsMatch(Path.GetFileName(path)));
         }
 
 
         void RemoveBom(string path)
         {
-            Trace.TraceInformation($"Removing UTF-8 BOM from {path}");
-            while (FileExtensions.DetectUtf8Bom(path) == true)
+            using (LogicalOperation.Start($"Removing UTF-8 BOM(s) from {path}"))
             {
-                FileExtensions.RemoveFirst(path, FileExtensions.Utf8Bom.Count);
+                while (FileExtensions.DetectUtf8Bom(path) == true)
+                {
+                    FileExtensions.RemoveFirst(path, FileExtensions.Utf8Bom.Count);
+                }
             }
         }
 
